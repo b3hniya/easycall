da28653e0667a99873f9ab16abc8ed2e652aefad
Behniya Azad <59304200+b3hniya@users.noreply.github.com>
2023-09-12 14:36:33 +0330

    HotFix: remove additional code for type testing.

--- lib/index.tsx
+++ lib/index.tsx
@@ -4,32 +4,3 @@ import { CallerProvider } from "./context/CallerContext"
 import React from "react"
 
 export { useCaller, createCaller, CallerProvider }
-
-const Main = () => {
-  return (
-    <CallerProvider
-      easyCallConfig={{
-        baseURL: "https://jsonplaceholder.typicode.com/",
-        apiRoutes: [
-          {
-            key: "todos",
-            method: "get",
-            endpoint: "todos",
-          },
-          {
-            key: "todo",
-            method: "get",
-            endpoint: "todo/:id",
-          },
-        ],
-      }}
-    >
-      <App />
-    </CallerProvider>
-  )
-}
-
-function App() {
-  const { data } = useCaller((caller) => caller.todos.get())
-  return <div>{data}</div>
-}
