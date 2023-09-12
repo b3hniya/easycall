ac6521ba34764e793771632927a61e8023259470
Behniya <b3hniya@gmail.com>
2023-09-12 14:38:17 +0330

    HotFix: remove react from imports.

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,5 @@
 import { createCaller } from "./helper/createCaller.helper"
 import { useCaller } from "./hooks/useCaller"
 import { CallerProvider } from "./context/CallerContext"
-import React from "react"
 
 export { useCaller, createCaller, CallerProvider }
