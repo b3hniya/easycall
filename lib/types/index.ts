94087828a84590a89e74697a05e5eaf7e6e130e4
Farzan <farzan.hosseiny@gmail.com>
2023-09-13 16:14:52 +0330

    Feature: index.ts file to export types from type folder

---
+++ lib/types/index.ts
@@ -0,0 +1,30 @@
+import {
+  TokenConfig,
+  ResponseDataSchema,
+  RetryConfig,
+  OnBeforeRequest,
+  OnError,
+  OnAfterResponse,
+  ApiRoute,
+  EasyCallInstanceConfig,
+} from "./EasycallInstance.type"
+
+import {
+    Params,
+    Caller,
+    Callers
+} from "./Caller.type"
+
+export {
+  TokenConfig,
+  ResponseDataSchema,
+  RetryConfig,
+  OnBeforeRequest,
+  OnError,
+  OnAfterResponse,
+  ApiRoute,
+  EasyCallInstanceConfig,
+  Params,
+  Caller,
+  Callers
+}
