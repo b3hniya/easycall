a02a902c54a46a6a8098eeb5f66caa9f06fe3229
Behniya <b3hniya@gmail.com>
2023-09-13 20:51:27 +0330

    Fix: refactor createCaller.

--- lib/helper/createCaller.helper.ts
+++ lib/helper/createCaller.helper.ts
@@ -2,46 +2,53 @@ import { apiMethods } from "../constants"
 import { Callers } from "../types/Caller.type"
 import { AxiosInstance, InternalAxiosRequestConfig } from "axios"
 import { generateAxiosInstance } from "./generateAxiosInstance.helper"
-import { ApiRoute, EasyCallInstanceConfig } from "../types/EasycallInstance.type"
-
-const generateApiMethod = (callers: Callers, route: ApiRoute, axiosInstance: AxiosInstance) => {
-  const { key, endpoint, method } = route
-
-  callers[key] = {
-    ...callers[key],
-    [method]: apiMethods[method].bind(null, axiosInstance, endpoint), // binding the axiosInstance and endpoint so they're pre-filled for each call
-  }
-}
-
-export const generateApiMethodsBasedOnCallerConfig = (
-  apiRoutes: ApiRoute[],
-  axiosInstance: AxiosInstance,
-) => {
-  const callers: Callers = {}
-  apiRoutes.forEach((route) => generateApiMethod(callers, route, axiosInstance))
-  return callers
-}
+import { EasyCallInstanceConfig } from "../types/EasycallInstance.type"
 
 export type AxiosOnBeforeRequest = (
   conf: InternalAxiosRequestConfig<any>,
 ) => InternalAxiosRequestConfig<any>
 
-export const createCaller = (callerConfig: EasyCallInstanceConfig) => {
+export const generateCallers = (
+  callerConfig: EasyCallInstanceConfig,
+  axiosInstance: AxiosInstance,
+) => {
+  const callers: Callers = {}
+
+  if (callerConfig.apiRoutes) {
+    callerConfig.apiRoutes.forEach(({ key, endpoint, method }) => {
+      callers[key] = {
+        ...callers[key],
+        [method]: apiMethods[method].bind(null, axiosInstance, endpoint),
+      }
+    })
+  }
+
+  return callers
+}
+
+export const createAxiosInstance = (callerConfig: EasyCallInstanceConfig) => {
   const axiosInstance = generateAxiosInstance(callerConfig)
 
-  axiosInstance.interceptors.request.use(
-    callerConfig?.onBeforeRequest as AxiosOnBeforeRequest,
-    callerConfig?.onBeforeRequestError,
-  )
+  if (callerConfig?.onBeforeRequest || callerConfig?.onBeforeRequestError) {
+    axiosInstance.interceptors.request.use(
+      callerConfig.onBeforeRequest as AxiosOnBeforeRequest,
+      callerConfig.onBeforeRequestError,
+    )
+  }
 
-  axiosInstance.interceptors.response.use(
-    callerConfig?.onAfterResponse,
-    callerConfig?.onAfterResponseError,
-  )
+  if (callerConfig?.onAfterResponse || callerConfig?.onAfterResponseError) {
+    axiosInstance.interceptors.response.use(
+      callerConfig.onAfterResponse,
+      callerConfig.onAfterResponseError,
+    )
+  }
 
-  const callers: Callers = callerConfig.apiRoutes
-    ? generateApiMethodsBasedOnCallerConfig(callerConfig.apiRoutes, axiosInstance)
-    : {}
+  return axiosInstance
+}
+
+export const createCaller = (callerConfig: EasyCallInstanceConfig) => {
+  const axiosInstance = createAxiosInstance(callerConfig)
+  const callers = generateCallers(callerConfig, axiosInstance)
 
   return {
     axiosInstance,
